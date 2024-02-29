32f2a1942310827b7fd1e90e6e0c2e0ca38ca443
SwagSek <lolinz666@gmail.com>
2024-02-29 15:13:36 +0200

    Added get/set, constructor, tostring function to Student

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,55 @@
 package model;
 
 public class Student {
+	private long stID;
+	private String name;
+	private String surname;
+	private static long counter = 0;
+	
+	public long getStID() {
+		return stID;
+	}
+	
+	public void setStID() {
+		stID = counter;
+		counter++;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		if(name != null && name.matches("[A-Z]{1}[a-z]+")) {
+			this.name = name;
+		}
+		else this.name = "Undefined";
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		if(surname != null && surname.matches("[A-Z]{1}[a-z]+")) {
+			this.surname = surname;
+		}
+		else this.surname = "Undefined";
+	}
 
+	public Student() {
+		setStID();
+		setName("");
+		setSurname("");
+	}
+	
+	public Student(String name, String surname) {
+		setStID();
+		setName(name);
+		setSurname(surname);
+	}
+
+	public String toString() {
+		return "Student [stID=" + stID + ", name=" + name + ", surname=" + surname + "]";
+	}
 }
