51cd3333b9801176ea3b56d85c49dcd4ba283d11
SwagSek <lolinz666@gmail.com>
2024-03-07 13:20:53 +0200

    Add: ArrayLists

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.ArrayList;
+
 import model.Course;
 import model.Degree;
 import model.Grade;
@@ -8,26 +10,47 @@ import model.Student;
 
 public class MainService {
 
+	private static ArrayList<Professor> allProfessors = new ArrayList<Professor>();
+	private static ArrayList<Student> allStudents = new ArrayList<Student>();
+	private static ArrayList<Course> allCourses = new ArrayList<Course>();
+	private static ArrayList<Grade> allGrades = new ArrayList<Grade>();
+	
 	public static void main(String[] args) {
 		Professor pr1 = new Professor();
 		Professor pr2 = new Professor("Karina", "Skirmante", Degree.mg);
-		System.out.println(pr1);
-		System.out.println(pr2);
+		allProfessors.add(pr1);
+		allProfessors.add(pr2);
+		for(Professor professor : allProfessors) {
+			System.out.println(professor);
+		}
 		
 		Student stu1 = new Student();
 		Student stu2 = new Student("Klavs", "Grinvalds");
-		System.out.println(stu1);
-		System.out.println(stu2);
+		
+		allStudents.add(stu1);
+		allStudents.add(stu2);
+		for(Student student : allStudents) {
+			System.out.println(student);
+		}
 		
 		Course c1 = new Course();
 		Course c2 = new Course("Programmesana timekli JAVA", 4, pr2);
-		System.out.println(c1);
-		System.out.println(c2);
+		allCourses.add(c1);
+		allCourses.add(c2);
+		for(Course course : allCourses) {
+			System.out.println(course);
+		}
 		
 		Grade g1 = new Grade();
 		Grade g2 = new Grade(10, stu2, c2);
-		System.out.println(g1);
+		Grade g3 = new Grade(7, stu2, c1);
+		allGrades.add(g1);
+		allGrades.add(g2);
+		allGrades.add(g3);
 		System.out.println(g2);
+		for(Grade grade : allGrades) {
+			System.out.println(grade);
+		}
 	}
 
 }
